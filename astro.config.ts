2ef74d3e738bbe7c5c9ba876a452c409f8c7355f
Matt Franchi <mattwfranc@gmail.com>
2025-01-30 20:35:19 -0500

    redirect to cornell domain

--- astro.config.ts
+++ astro.config.ts
@@ -4,8 +4,8 @@ import UnoCSS from 'unocss/astro';
 
 export default defineConfig({
   // used to generate images
-  site: 'https://far-lab.github.io/', 
-  base: '/robotability/',
+  site: 'https://robotability.cornell.edu', 
+  base: '',
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
   vite: {
